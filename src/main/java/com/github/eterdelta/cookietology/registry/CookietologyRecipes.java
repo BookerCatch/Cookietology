fe282a95eb78fe3fc18a5a93970bb888eadc0a53
SilverCubess <67644822+SilverCubess@users.noreply.github.com>
2022-01-17 02:24:30 -0500

    Fix dough brilliance recipe ID

--- src/main/java/com/github/eterdelta/cookietology/registry/CookietologyRecipes.java
+++ src/main/java/com/github/eterdelta/cookietology/registry/CookietologyRecipes.java
@@ -22,7 +22,7 @@ public class CookietologyRecipes {
     public static final RegistryObject<RecipeSerializer<MixingRecipe>> MIXING_SERIALIZER = RECIPE_SERIALIZERS.register("mixing", MixingRecipe.Serializer::new);
     public static final RegistryObject<RecipeSerializer<DoughCompressingRecipe>> DOUGH_COMPRESSING_SERIALIZER = RECIPE_SERIALIZERS.register("crafting_special_doughcompressing", () -> new SimpleRecipeSerializer<>(DoughCompressingRecipe::new));
     public static final RegistryObject<RecipeSerializer<DoughMoldingRecipe>> DOUGH_MOLDING_SERIALIZER = RECIPE_SERIALIZERS.register("crafting_special_doughmolding", () -> new SimpleRecipeSerializer<>(DoughMoldingRecipe::new));
-    public static final RegistryObject<RecipeSerializer<DoughBrillianceRecipe>> DOUGH_BRILLIANCE_SERIALIZER = RECIPE_SERIALIZERS.register("crafting_special_doughbrilliance", () -> new SimpleRecipeSerializer<>(DoughBrillianceRecipe::new));
+    public static final RegistryObject<RecipeSerializer<DoughBrillianceRecipe>> DOUGH_BRILLIANCE_SERIALIZER = RECIPE_SERIALIZERS.register("mixing_special_doughbrilliance", () -> new SimpleRecipeSerializer<>(DoughBrillianceRecipe::new));
     public static final RegistryObject<RecipeSerializer<CookieBakingRecipe>> COOKIE_BAKING_SERIALIZER = RECIPE_SERIALIZERS.register("baking_special_cookie", () -> new SimpleRecipeSerializer<>(CookieBakingRecipe::new));
 
     public static final RecipeType<IMixingRecipe> MIXING = RecipeType.register(Cookietology.MODID + ":mixing");
